21fc632b2e04aef2cd2fd6ccb08d7f2fa6ec9154
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-03-15 16:14:10 -0600

    add code

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Welcome from '../Welcome';
 import FineArts from '../Fine_Arts';
+import Code from '../Code';
 
 export const Routes = () => {
   return (
     <div>
       <Route path="/" component={ Welcome }/>
       <Route path="/finearts" component={ FineArts }/>
+      <Route path="/code" component={ Code }/>
     </div>
   );
 };
