f7cfd951210fc61fd1a110e2e653fa085fcb6968
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-03-20 07:19:40 -0600

    add and route welcome

---
+++ src/components/CodePages/Welcome/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>WELCOME</div>
+    );
+  }
+}
+
+export default Welcome;
--- src/components/CodeRoutes/index.js
+++ src/components/CodeRoutes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import Welcome from '../CodePages/Welcome';
 import Hyperspace from '../CodePages/Hyperspace';
 import Headcount from '../CodePages/Headcount';
 import CodeNav from '../CodeNav';
@@ -8,6 +9,7 @@ export const CodeRoutes = () => {
   return (
     <div>
       <Route path="/code" component={ CodeNav }/>
+      <Route exact path="/code" component={ Welcome }/>
       <Route path="/code/hyperspace" component={ Hyperspace }/>
       <Route path="/code/headcount" component={ Headcount }/>
     </div>
