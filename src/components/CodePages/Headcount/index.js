d306f0bbe0bfbf8d23e66a033aa1686475dc8807
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-03-26 19:37:22 -0600

    add page title and movie tracker description

--- src/components/CodePages/Headcount/index.js
+++ src/components/CodePages/Headcount/index.js
@@ -64,15 +64,13 @@ class Headcount extends Component {
         </section>
         <section>
           <div className="page-title">HEADCOUNT</div>
-          <div className>
-            <p>{`It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.`}</p>
-          </div>
           <button
             className="repository-button"
             onClick={this.goGitHub}>Repository Page</button>
           <section className="project-section">
             {this.displaySet0()}
             <p>{`It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.`}</p>
+            <p>{`It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.`}</p>
             {this.displaySet1()}
           </section>
         </section>
--- src/components/CodePages/MovieTracker/index.js
+++ src/components/CodePages/MovieTracker/index.js
@@ -49,6 +49,9 @@ class MovieTracker extends Component {
             className="technology-icon"
             src={require("../../../assets/technologies/jslogo.svg")}/>
           <img
+            className="technology-icon"
+            src={require("../../../assets/technologies/es6logo.svg")}/>
+          <img
             className="technology-icon react-icon"
             src={require("../../../assets/technologies/reactlogo.svg")}/>
           <img
@@ -61,6 +64,9 @@ class MovieTracker extends Component {
             className="technology-icon sass-icon"
             src={require("../../../assets/technologies/sasslogo.svg")}/>
           <img
+            className="technology-icon"
+            src={require("../../../assets/technologies/jestlogo.svg")}/>
+          <img
             className="technology-icon git-icon"
             src={require("../../../assets/technologies/gitLogo.svg")}/>
           <img
@@ -69,15 +75,46 @@ class MovieTracker extends Component {
         </section>
         <section>
           <div className="page-title">MOVIE TRACKER</div>
-          <div className>
-            <p>{`It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.`}</p>
-          </div>
           <button
             className="repository-button"
             onClick={this.goGitHub}>Repository Page</button>
           <section className="project-section">
             {this.displaySet0()}
-            <p>{`It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.`}</p>
+            <div className="project-description">
+              <p className="paragraph-title">Synopsis</p>
+              <p className="page-paragraph">{`Movie Tracker is a group project that works off The Movie DB API and incorporates our own API to save user information. We had one week to build an application that pulled in the most recent movies from The Movie DB API. Our API allows a user to register and then log in when they return.`}</p>
+              <p className="page-paragraph">{`A user can favorite movies, which is then saved in our API so that user can view their list when they log in again. We also did a project extension where a user can click and view more details about any individual movie. The application was built in React and Redux. Testing suites were built using Jest and Enzyme.`}</p>
+            </div>
+            <div className="project-description">
+              <p className="paragraph-title">Project Goals</p>
+              <p
+                className="page-paragraph">
+                {`• Create modular React components`}</p>
+              <p
+                className="page-paragraph">
+                {`• Connect components to Redux store`}</p>
+              <p
+                className="page-paragraph">
+                {`• Test components from both a unit and acceptance standpoint`}</p>
+              <p
+                className="page-paragraph">
+                {`• Design an appealing and user friendly app`}</p>
+            </div>
+            <div className="project-description">
+              <p className="paragraph-title">Contributors</p>
+              <p
+                className="page-paragraph">
+                Jorge Ed PerezGa - https://github.com/JorgeEdPerezGa</p>
+              <p
+                className="page-paragraph">
+                Amanda Tjan - https://github.com/soytjan</p>
+              <p
+                className="page-paragraph">
+                James Logue - https://github.com/jjlljj</p>
+            </div>
+            <div className="project-description">
+              <p className="paragraph-title">Project Layout</p>
+            </div>
             {this.displaySet1()}
           </section>
         </section>
