806bf340c592ea57a83f73e59779c5ee52d1cb75
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-03-27 17:25:00 -0600

    add design welcome

---
+++ src/components/DesignPages/Welcome/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+class Welcome extends Component {
+
+  render() {
+    return (
+      <div>
+        <section>
+          <p className="main-page-title">PROJECTS</p>
+        </section>
+        <section className="gallery-section">
+          <article className="gallery-article">
+            <p className="main-page-project-title">BATMAN</p>
+            <Link to={"/design/comp-one"}>
+              <div className="batman-image gallery-image"></div>
+            </Link>
+          </article>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
