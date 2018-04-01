b2a8485c7e534e07c728a8ea60966be3c8377271
Jorge Ed PerezGa - Turing <perezga@icloud.com>
2018-03-31 19:53:19 -0600

    add contact initial template

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class Contact extends Component{
   render() {
     return (
-      <div>CONTACT</div>
+      <div>
+        <section>
+          <p className="main-page-title">CONTACT</p>
+        </section>
+        <section className="contact-left">
+          <article className="contact-icon-and-info">
+            <img
+              className="contact-icon"
+              src={require("../../assets/contact/telephone.svg")}/>
+            <p className="contact-info">(720) 979 8944</p>
+          </article>
+        </section>
+      </div>
     );
   }
 }
